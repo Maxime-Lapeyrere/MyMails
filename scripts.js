d62e09b9b1a6112011f7bd733b4b12e750becb5f
Maxime Lapeyrere <lapeyreremax@gmail.com>
2021-01-07 12:13:55 +0100

    MyMails apres fin part 2

---
+++ scripts.js
@@ -0,0 +1,13 @@
+document.getElementById('compteur').textContent = document.getElementsByClassName('mail').length;
+
+
+var trashClick = document.getElementsByClassName('bin');
+
+for(var i=0; i<trashClick.length; i++){
+trashClick[i].addEventListener("click", 
+    function(){
+            this.parentNode.remove();
+            document.getElementById('compteur').textContent = document.getElementsByClassName('mail').length;
+        }
+    )
+}
