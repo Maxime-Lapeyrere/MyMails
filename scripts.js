145ab958d4dd0f08928f50cb979f827e227e7543
Maxime Lapeyrere <lapeyreremax@gmail.com>
2021-01-08 18:37:48 +0100

    fin part4bis

--- scripts.js
+++ scripts.js
@@ -1,10 +1,7 @@
 $('#compteur').text($('.mail').length); 
 
 
-
-var trashClick = $('.bin');
-
-trashClick.click(
+$('.bin').click(
     function(){
         this.parentNode.remove();
         $('#compteur').text($('.mail').length);
@@ -12,145 +9,49 @@ trashClick.click(
     )
 
 
-// for(var i=0; i<trashClick.length; i++){
-// trashClick[i].addEventListener("click", 
-//     function(){
-//             this.parentNode.remove();
-//             document.getElementById('compteur').textContent = document.getElementsByClassName('mail').length;
-//         }
-//     )
-// }
 
 var clickadd = $('#btn');
 
 clickadd.click(
     function(){
-        var newtxt = $('#newmsg').val()
-        
-        var ul = $("#list")
-        
-        var newli = document.createElement("li");
-        
-        var first = $('.mail')
-        console.log('info',first)
-        
-        newli.setAttribute("class","mail tmplt")
-        
-        ul.append(newli);
-        
-
         function getRandomInt(max) {
             return Math.floor(Math.random() * Math.floor(max)); }
         var selectrandom = getRandomInt(11)
         tabimg = new Array();
         tabimg = ["./img/Fichier 1.png","./img/Fichier 2.png","./img/Fichier 3.png","./img/Fichier 4.png","./img/Fichier 5.png","./img/Fichier 6.png","./img/Fichier 7.png","./img/Fichier 8.png","./img/Fichier 9.png", "./img/Fichier 10.png","./img/Fichier 11.png","./img/Fichier 12.png",] 
         tabname = new Array();
-        tabname = []
-        var imag =  document.createElement("img");
-            imag.className = "avatar";
-            imag.src = tabimg[selectrandom].source;
-        newli.appendChild(imag)
+        tabname = ["Jacob Granet","Ambre Corriveau","Yvon Messier","Julie Beauregard","Ilonna Bonnot","Aymeric Féret","Nina Choffard","Esther Allaire","Maria Lavigne","Jean-Loup Poincaré","Virginie De Guignes","Alain Marchand"]
+            var nomaleatoire = tabname[selectrandom]
+            var imgaleatoire = tabimg[selectrandom]
+            console.log(imgaleatoire)
+            
 
-        var nameMessage =  document.createElement("div");
-            nameMessage.setAttribute("class","message")
-        newli.appendChild(nameMessage);
 
-        var h6 =  document.createElement("h6");
-            h6.textContent = "Jean Ledu";
-        nameMessage.appendChild(h6);
 
-        var content = document.createElement('p');
-            content.textContent = newtxt  
-        nameMessage.appendChild(content)
-
-        var trash = document.createElement('img')
-            trash.src = "./img/recycle-bin-1-461646.png";
-            trash.setAttribute("class","bin")  
-        newli.appendChild(trash)
-
+        $(".newtxt").after(`<li class='mail tmplt'><img class='avatar' src='${imgaleatoire}' alt='photo de profil Noel'><div class='message'><h6>${nomaleatoire}</h6><p>${$("#newmsg").val()}</p></div><img class='bin' src='./img/recycle-bin-1-461646.png' alt='bin picto'></li>`)
+        $("#newmsg").val('Nouveau Message')
         
-
-        for(var i=0; i<trashClick.length; i++){
-            trashClick[i].addEventListener("click", 
-                function(){
-                        this.parentNode.remove();
-                        $('#compteur').text($('.mail').length);
-                    }
-                )
-            }
-        
-        newli.insertAfter(".newtxt")
         $('#compteur').text($('.mail').length);
-        document.getElementById('newmsg').value = 'Nouveau Message'
+        $('.bin').click(
+            function(){
+                this.parentNode.remove();
+                $('#compteur').text($('.mail').length);
+                }
+            )
+            $("#newmsg").value=''
         
-    }
+    })
+
+    $('#btn1').click(function(){
+        $('h6').each(
+            function(){
+                if($('#search').val()!= $(this).text()){
+                    $(this).parent().parent().fadeOut()
+                }
+            }
+        )
+    }     
 )
-
-
-
-// clickadd.addEventListener("click", 
-//     function(){
-//         var newtxt = document.getElementById('newmsg').value
-//         console.log(newtxt)
-//         var ul = document.getElementById("list")
-//         var newli = document.createElement("li");
-//         var first = document.getElementsByClassName('mail')
-        
-        
-//         newli.setAttribute("class","mail tmplt")
-        
-//         ul.appendChild(newli);
-
-//         function getRandomInt(max) {
-//             return Math.floor(Math.random() * Math.floor(max)); }
-//         tabimg = new Array();
-//         tabimg = ["./img/Fichier 1.png","./img/Fichier 2.png","./img/Fichier 3.png","./img/Fichier 4.png","./img/Fichier 5.png","./img/Fichier 6.png","./img/Fichier 7.png","./img/Fichier 8.png","./img/Fichier 9.png","./img/Fichier 10.png","./img/Fichier 11.png","./img/Fichier 12.png"] 
-
-//         var imag =  document.createElement("img");
-//             imag.className = "avatar";
-//             imag.src = tabimg[getRandomInt(11)];
-//         newli.appendChild(imag)
-
-//         var nameMessage =  document.createElement("div");
-//             nameMessage.setAttribute("class","message")
-//         newli.appendChild(nameMessage);
-
-//         var h6 =  document.createElement("h6");
-//             h6.textContent = "Jean Ledu";
-//         nameMessage.appendChild(h6);
-
-//         var content = document.createElement('p');
-//             content.textContent = newtxt  
-//         nameMessage.appendChild(content)
-
-//         var trash = document.createElement('img')
-//             trash.src = "./img/recycle-bin-1-461646.png";
-//             trash.setAttribute("class","bin")  
-//         newli.appendChild(trash)
-
-        
-
-//         for(var i=0; i<trashClick.length; i++){
-//             trashClick[i].addEventListener("click", 
-//                 function(){
-//                         this.parentNode.remove();
-//                         $('#compteur').textContent = $('.mail').length;
-//                     }
-//                 )
-//             }
-        
-//         document.getElementById('list').insertBefore(newli, first[0])
     
-//         $('#compteur').textContent = $('.mail').length;
-//         document.getElementById('newmsg').value = 'Nouveau Message'
-        
-//     }
-// )
 
-// $('#compteur').textContent = $('.mail').length;
 
-// var sent = document.getElementById("newmsg").value;
-// var sent1 = document.getElementById("newmsg").textContent;
-// sent.textContent=('Nouveau Message')
-// console.log('info',sent1)
-// console.log('info2',sent)
